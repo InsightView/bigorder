a47f0adf016048082a98bd0c2bf80c4d7e24d976
dyllanwli <diya.li03@icloud.com>
2019-06-12 07:14:03 +0800

    add mogoose schema test

--- db/create-db/mongodb.js
+++ db/create-db/mongodb.js
@@ -1,16 +1,33 @@
 'use strict'
-let url = 'mongodb://localhost/bigorder_db';
-let mongoose = require('mongoose'); mongoose.connect(url);
+let config = require('./config.json');
+let remoteHost = config.remote_host;
+let remoteDbName = config.remote_db_name;
+let tmpCollection = config.tmp_collection;
 
-let Schema = mongoose.Schema;
-let userSchema = new Schema({
-    id: String,
-    name: String,
-    pass: String,
-    email: String,
-    createTime: Date,
-    isDelete: Boolean,    
-    loginHistory: [{type: Date}]       
-}, {collection: 'bigorder_user'});
+let url = 'mongodb://' + remoteHost + "/" + remoteDbName;
 
-let bigorder_user = mongoose.model('bigorder_user', userSchema);
+
+let mongoose = require('mongoose');
+mongoose.connect(url, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log(remoteDbName, "connected.");
+    let Schema = mongoose.Schema;
+    let userSchema = new Schema({
+        id: String,
+        name: String,
+        pass: String,
+        email: String,
+        createTime: Date,
+        isDelete: Boolean,
+        loginHistory: [{ type: Date }]
+    }, { collection: tmpCollection });
+    let schemaModel = mongoose.model(tmpCollection, userSchema);
+    let test = new schemaModel({ 'name': 'test_name' });
+    console.log(test);
+});
+
+
+
+
